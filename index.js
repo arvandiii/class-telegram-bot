b99cf0ae2f7c30e34d371817f153ad763988e4d3
alirezaarv <alireza.arv.mail@gmail.com>
2018-01-19 17:33:26 +0330

    bug fix

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const token = '454088608:AAFIBePz5j6dBAzX4cNIM5Tz8xIDcTipsgg'
 const bot = new TelegramBot(token, { polling: true })
 
 bot.on('message', msg => {
+  const chatId = msg.chat.id
   bot.sendMessage(chatId, 'bot is working')
 })
